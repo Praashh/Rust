b5430bce9fbfe55fb26de581d4e11d51e4ff56f4
Prashant Varma <prashantvarma5083@gmail.com>
2024-03-01 19:52:11 +0530

    Chapter 2 Completed

---
+++ hello_world_cargo/target/debug/deps/hello_world_cargo-15ab23a2c88d80e0.d
@@ -0,0 +1,5 @@
+/home/praash/Desktop/Rust/hello_world_cargo/target/debug/deps/hello_world_cargo-15ab23a2c88d80e0: src/main.rs
+
+/home/praash/Desktop/Rust/hello_world_cargo/target/debug/deps/hello_world_cargo-15ab23a2c88d80e0.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world_cargo/target/debug/hello_world_cargo.d
@@ -0,0 +1,1 @@
+/home/praash/Desktop/Rust/hello_world_cargo/target/debug/hello_world_cargo: /home/praash/Desktop/Rust/hello_world_cargo/src/main.rs
